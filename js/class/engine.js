f505b994fc4ee9fe4b430ddee65a7ccb8e1d4592
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-12-10 23:23:26 +0200

    create shuffled list of card sets

--- js/class/engine.js
+++ js/class/engine.js
@@ -2,6 +2,17 @@ var Engine = function(global) {
   this.doc = global.document;
 };
 
+// initialize the game board
 Engine.prototype.init = function() {
+  // create shuffled list
+  var cardSet = [];
+  for (var i=0; i<8; i++) {
+    cardSet.push(i);
+    cardSet.push(i);
+  }
+  cardSet.sort(function(){ return 0.5 - Math.random(); });
+
+  // build cards
+
 
 };
