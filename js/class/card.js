337bf09497728614b390ba8a032405220f66f6b9
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2017-12-11 12:21:50 +0200

    create card side elements with document.createElement()

--- js/class/card.js
+++ js/class/card.js
@@ -4,8 +4,19 @@ let Card = function(setId) {
 
 // create div element containing the card
 Card.prototype.createElement = function() {
+  // create card sides container
   const el = document.createElement('div');
   el.className = 'card';
-  el.innerHTML = '<div class="card-side card-face-' + this.setId +'"></div>';
+
+  // create card back element
+  const elBack = document.createElement('div');
+  elBack.className = 'card-side card-back';
+  el.appendChild(elBack);
+
+  // create card face element
+  const elFace = document.createElement('div');
+  elFace.className = 'card-side card-face-' + this.setId;
+  el.appendChild(elBack);
+
   return el;
 };
