cfd9c999bdc721e94a06314408254b88377e54af
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-12-12 00:14:00 +0200

    add comments on app initialiation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+// Create Engine instance
 let engine = new Engine(document);
 
+// Initialize game board
 engine.init();
 
+// Add event handler to a game over message button.
 document.getElementById('btn-restart').onclick = engine.restart.bind(engine);
