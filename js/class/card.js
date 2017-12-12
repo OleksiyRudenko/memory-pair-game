f993f57c234f3843926e28b6cac4ea9a477eac50
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2017-12-12 15:23:57 +0200

    mark obsolete code

--- js/class/card.js
+++ js/class/card.js
@@ -69,7 +69,7 @@ Card.prototype.onClick = function() {
   console.log('Clicked ' + this.elId);
   // flip on click only if isFaceDown
   if (this.isActive && this.isFaceDown) {
-    // document.getElementById(this.elId).classList.toggle('flip');
+    // obsolete: document.getElementById(this.elId).classList.toggle('flip');
     this.el.classList.toggle('flip');
     this.actionFlipOver(this.idIndex);
     this.isFaceDown = false;
@@ -86,7 +86,7 @@ Card.prototype.onClick = function() {
 Card.prototype.flipDown = function() {
   console.log('Flipping down ' + this.elId);
   if (this.isActive && !this.isFaceDown) {
-    // document.getElementById(this.elId).classList.toggle('flip');
+    // obsolete: document.getElementById(this.elId).classList.toggle('flip');
     this.el.classList.toggle('flip');
     this.isFaceDown = true;
   }
@@ -101,7 +101,7 @@ Card.prototype.flipDown = function() {
 Card.prototype.hide = function() {
   console.log('Hiding ' + this.elId);
   if (this.isActive) {
-    // document.getElementById(this.elFaceId).style.display = 'none';
+    // obsolete: document.getElementById(this.elFaceId).style.display = 'none';
     this.elFace.style.display = 'none';
     this.isActive = false;
   }
