99524810eff41b82636e8412cf14e572087689a0
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2017-12-14 19:55:15 +0200

    Migrate to ES6 classes
    Resolves #5

--- js/class/card.js
+++ js/class/card.js
@@ -5,192 +5,194 @@
  * @param {number} idIndex - card id index
  * @param {onCardFlipOver} actionFlipOver - a callback Engine.onCardFlipOver
  */
-let Card = function(setId, idIndex, actionFlipOver) {
-  this.setId = setId;
-  this.idIndex = idIndex;
-  this.elId = '';          // card element id
-  this.el = null;          // reference to card element
-  this.elFaceId = '';      // card face element id
-  this.elFace = null;      // reference to card face element
-  this.isActive = true;    // whether card is active (not removed yet)
-  this.isFaceDown = true;  // whether is card face down
-  this.actionFlipOver = actionFlipOver;
+class Card {
+  constructor(setId, idIndex, actionFlipOver) {
+    this.setId = setId;
+    this.idIndex = idIndex;
+    this.elId = '';          // card element id
+    this.el = null;          // reference to card element
+    this.elFaceId = '';      // card face element id
+    this.elFace = null;      // reference to card face element
+    this.isActive = true;    // whether card is active (not removed yet)
+    this.isFaceDown = true;  // whether is card face down
+    this.actionFlipOver = actionFlipOver;
 
-  // css transition management
-  this.isInAnimation = false; // indicates whether any animation (transition) runs
-  this.visualEffectQueue = [];   // transition and visibility change calls queue; array of {isAnimated:,action}
-};
+    // css transition management
+    this.isInAnimation = false; // indicates whether any animation (transition) runs
+    this.visualEffectQueue = [];   // transition and visibility change calls queue; array of {isAnimated:,action}
+  };
 
-/**
- * This creates div element containing the card.
- * @memberof Card
- * @name createElement
- * @function
- * @returns {Object} div element containing card
- */
-Card.prototype.createElement = function() {
-  // create card sides container
-  const el = document.createElement('div');
-  el.className = 'card';
-  this.elId = 'card-' + this.idIndex;
-  el.id = this.elId;
+  /**
+   * This creates div element containing the card.
+   * @memberof Card
+   * @name createElement
+   * @function
+   * @returns {Object} div element containing card
+   */
+  createElement() {
+    // create card sides container
+    const el = document.createElement('div');
+    el.className = 'card';
+    this.elId = 'card-' + this.idIndex;
+    el.id = this.elId;
 
-  // create flipper div
-  elFlipper = document.createElement('div');
-  elFlipper.className = 'flipper';
+    // create flipper div
+    const elFlipper = document.createElement('div');
+    elFlipper.className = 'flipper';
 
-  // create card back element
-  const elBack = document.createElement('div');
-  elBack.className = 'card-side card-back';
-  elFlipper.appendChild(elBack);
+    // create card back element
+    const elBack = document.createElement('div');
+    elBack.className = 'card-side card-back';
+    elFlipper.appendChild(elBack);
 
-  // create card face element
-  const elFace = document.createElement('div');
-  elFace.className = 'card-side card-face card-face-' + this.setId;
-  this.elFaceId = 'card-face-id-' + this.idIndex;
-  elFace.id = this.elFaceId;
-  this.elFace = elFace;
-  elFlipper.appendChild(elFace);
+    // create card face element
+    const elFace = document.createElement('div');
+    elFace.className = 'card-side card-face card-face-' + this.setId;
+    this.elFaceId = 'card-face-id-' + this.idIndex;
+    elFace.id = this.elFaceId;
+    this.elFace = elFace;
+    elFlipper.appendChild(elFace);
 
-  // add click event handler
-  el.onclick = this.onClick.bind(this);
-  // attach ontouchstart="this.classList.toggle('flip');" event handler to deem swipes as clicks
-  el.ontouchstart = this.onClick.bind(this);
+    // add click event handler
+    el.onclick = this.onClick.bind(this);
+    // attach ontouchstart="this.classList.toggle('flip');" event handler to deem swipes as clicks
+    el.ontouchstart = this.onClick.bind(this);
 
     // complete container
-  el.appendChild(elFlipper);
+    el.appendChild(elFlipper);
 
-  // attach css transition listener
-  let transitionEndEvent = whichTransitionEndEvent();
-  transitionEndEvent && el.addEventListener(transitionEndEvent,
-    // console.log('Transition of ' + this.elId + ' complete!');
-    this.onTransitionEnd.bind(this)
-  );
-
-  // keep reference
-  this.el = el;
-
-  // console.log('Created ' + this.elId);
-  return this.el;
-};
-
-/**
- * Invoked whenever card animation (transition) ends.
- * Attached to card element in Card.init()
- * @memberof Card
- * @name onTransitionEnd
- * @function
- */
-Card.prototype.onTransitionEnd = function() {
-  // console.log('Transition of ' + this.elId + ' has ended!');
-  this.isInAnimation = false;
-  // if any method in queue, then invoke it
-  if (this.visualEffectQueue.length) {
-    let action = this.visualEffectQueue.shift(); // remove method from queue and invoke it
-    this.isInAnimation = action.isAnimated;
-    action.action();
-  }
-};
-
-/**
- * This queues action or executes it immediately if none is running.
- * @memberof Card
- * @name queueVisualEffect
- * @function
- * @param action - method to queue or execute
- * @param isAnimated - true if action invokes animation; false is of immediate effect
- */
-Card.prototype.queueVisualEffect = function(action, isAnimated) {
-  if (this.isInAnimation) {
-    // queue
-    this.visualEffectQueue.push({
-      isAnimated: isAnimated,
-      action: action,
-    });
-  } else {
-    // execute
-    this.isInAnimation = isAnimated;
-    action();
-  }
-};
-
-/**
- * This flips the card and calls Engine.onCardFlipOver callback.
- * Called by face element within card element event handler whenever card face is clicked.
- * @memberof Card
- * @name onClick
- * @function
- */
-Card.prototype.onClick = function() {
-  // console.log('Clicked ' + this.elId);
-  // flip on click only if isFaceDown
-  if (this.isActive && this.isFaceDown) {
-    this.isFaceDown = false;
-    // first: flip the card
-    this.queueVisualEffect(
-      () => {
-        this.el.classList.toggle('flip');
-      },
-      true
+    // attach css transition listener
+    let transitionEndEvent = whichTransitionEndEvent();
+    transitionEndEvent && el.addEventListener(transitionEndEvent,
+      // console.log('Transition of ' + this.elId + ' complete!');
+      this.onTransitionEnd.bind(this)
     );
-    // second: invoke engine callback upon flip is completed
-    this.queueVisualEffect(
-      () => {
-        this.actionFlipOver(this.idIndex);
-      },
-      false
-    );
-  }
-};
 
-/**
- * This flips card back face down.
- * Called by Engine.onCardFlipOver()
- * @memberof Card
- * @name flipDown
- * @function
- */
-Card.prototype.flipDown = function() {
-  // console.log('Flipping down ' + this.elId);
-  if (this.isActive && !this.isFaceDown) {
-    // flip card face down
-    this.queueVisualEffect(
-      () => {
-        this.el.classList.toggle('flip');
-      },
-      true
-    );
-    // make card clickable only when it is completely flipped face down
-    this.queueVisualEffect(
-      () => {
-        this.isFaceDown = true;
-      },
-      false
-    );
-  }
-};
+    // keep reference
+    this.el = el;
 
-/**
- * This hides div element containing the card.
- * @memberof Card
- * @name hide
- * @function
- */
-Card.prototype.hide = function() {
-  // console.log('Hiding ' + this.elId);
-  if (this.isActive) {
-    this.isActive = false;
-    this.queueVisualEffect(
-      () => {
-        this.elFace.className += ' fade-out';
-      },
-      true
-    );
-    this.queueVisualEffect(
-      () => {
-        this.elFace.style.display = 'none';
-      },
-      false
-    );
-  }
-};
+    // console.log('Created ' + this.elId);
+    return this.el;
+  };
+
+  /**
+   * Invoked whenever card animation (transition) ends.
+   * Attached to card element in Card.init()
+   * @memberof Card
+   * @name onTransitionEnd
+   * @function
+   */
+  onTransitionEnd() {
+    // console.log('Transition of ' + this.elId + ' has ended!');
+    this.isInAnimation = false;
+    // if any method in queue, then invoke it
+    if (this.visualEffectQueue.length) {
+      let action = this.visualEffectQueue.shift(); // remove method from queue and invoke it
+      this.isInAnimation = action.isAnimated;
+      action.action();
+    }
+  };
+
+  /**
+   * This queues action or executes it immediately if none is running.
+   * @memberof Card
+   * @name queueVisualEffect
+   * @function
+   * @param action - method to queue or execute
+   * @param isAnimated - true if action invokes animation; false is of immediate effect
+   */
+  queueVisualEffect(action, isAnimated) {
+    if (this.isInAnimation) {
+      // queue
+      this.visualEffectQueue.push({
+        isAnimated: isAnimated,
+        action: action,
+      });
+    } else {
+      // execute
+      this.isInAnimation = isAnimated;
+      action();
+    }
+  };
+
+  /**
+   * This flips the card and calls Engine.onCardFlipOver callback.
+   * Called by face element within card element event handler whenever card face is clicked.
+   * @memberof Card
+   * @name onClick
+   * @function
+   */
+  onClick() {
+    // console.log('Clicked ' + this.elId);
+    // flip on click only if isFaceDown
+    if (this.isActive && this.isFaceDown) {
+      this.isFaceDown = false;
+      // first: flip the card
+      this.queueVisualEffect(
+        () => {
+          this.el.classList.toggle('flip');
+        },
+        true
+      );
+      // second: invoke engine callback upon flip is completed
+      this.queueVisualEffect(
+        () => {
+          this.actionFlipOver(this.idIndex);
+        },
+        false
+      );
+    }
+  };
+
+  /**
+   * This flips card back face down.
+   * Called by Engine.onCardFlipOver()
+   * @memberof Card
+   * @name flipDown
+   * @function
+   */
+  flipDown() {
+    // console.log('Flipping down ' + this.elId);
+    if (this.isActive && !this.isFaceDown) {
+      // flip card face down
+      this.queueVisualEffect(
+        () => {
+          this.el.classList.toggle('flip');
+        },
+        true
+      );
+      // make card clickable only when it is completely flipped face down
+      this.queueVisualEffect(
+        () => {
+          this.isFaceDown = true;
+        },
+        false
+      );
+    }
+  };
+
+  /**
+   * This hides div element containing the card.
+   * @memberof Card
+   * @name hide
+   * @function
+   */
+  hide() {
+    // console.log('Hiding ' + this.elId);
+    if (this.isActive) {
+      this.isActive = false;
+      this.queueVisualEffect(
+        () => {
+          this.elFace.className += ' fade-out';
+        },
+        true
+      );
+      this.queueVisualEffect(
+        () => {
+          this.elFace.style.display = 'none';
+        },
+        false
+      );
+    }
+  };
+}
--- js/class/engine.js
+++ js/class/engine.js
@@ -3,142 +3,146 @@
  * @constructor
  * @param {Object} doc - reference to document global
  */
-let Engine = function(doc) {
-  this.doc = doc;
-  this.gameBoard = this.doc.getElementById('game-board'); // refers to game-board container
-  this.cardSet = [];          // stores instances of Card class
-  this.flippedCardQueue = []; // stores flipped cards
-  this.clickCount = 0;        // counts flipped cards
-};
+class Engine {
+  constructor(doc) {
+    this.doc = doc;
+    this.gameBoard = this.doc.getElementById('game-board'); // refers to game-board container
+    this.cardSet = [];          // stores instances of Card class
+    this.flippedCardQueue = []; // stores flipped cards
+    this.clickCount = 0;        // counts flipped cards
+  };
 
-/**
- * This intializes a game board.
- * @memberof Engine
- * @name init
- * @function
- */
-Engine.prototype.init = function() {
-  // reset click count
-  this.clickCount = 0;
+  /**
+   * This intializes a game board.
+   * @memberof Engine
+   * @name init
+   * @function
+   */
+  init() {
+    // reset click count
+    this.clickCount = 0;
 
-  // create shuffled list
-  let cardSetIdList = [];
-  for (let i=0; i<8; i++) {
-    cardSetIdList.push(i, i);
-  }
-  cardSetIdList.sort(function(){ return 0.5 - Math.random(); });
+    // create shuffled list
+    let cardSetIdList = [];
+    for (let i = 0; i < 8; i++) {
+      cardSetIdList.push(i, i);
+    }
+    cardSetIdList.sort(function () {
+      return 0.5 - Math.random();
+    });
 
-  // build cards
-  const subContainer = this.doc.createElement('div');
-  subContainer.className = 'game-board';
-  let cardSet = [];
-  const onCardFlipOver = this.onCardFlipOver;
-  let self = this;
-  // console.log(this.onCardFlipOver);
-  cardSetIdList.forEach(function(cardSetId, index) {
-    const card = new Card(cardSetId, index, onCardFlipOver.bind(self));
-    cardSet.push(card);
-    subContainer.appendChild(card.createElement());
-  });
-  this.cardSet = cardSet;
-  // attach container to a game board container
-  this.gameBoard.appendChild(subContainer);
-};
+    // build cards
+    const subContainer = this.doc.createElement('div');
+    subContainer.className = 'game-board';
+    let cardSet = [];
+    const onCardFlipOver = this.onCardFlipOver;
+    let self = this;
+    // console.log(this.onCardFlipOver);
+    cardSetIdList.forEach(function (cardSetId, index) {
+      const card = new Card(cardSetId, index, onCardFlipOver.bind(self));
+      cardSet.push(card);
+      subContainer.appendChild(card.createElement());
+    });
+    this.cardSet = cardSet;
+    // attach container to a game board container
+    this.gameBoard.appendChild(subContainer);
+  };
 
-/**
- * This is a callback employed by a Card instance when the latter is being clicked.
- * Called by Card.onClick()
- * @memberof Engine
- * @name onCardFlipOver
- * @function
- * @param {number} idIndex - card id index
- */
-Engine.prototype.onCardFlipOver = function(idIndex) {
-  this.clickCount++;
-  // Add card to the queue.
-  // console.log('Card #' + idIndex + ' flipped face up.');
-  this.flippedCardQueue.push(idIndex);
-  // console.log(this.flippedCardQueue);
+  /**
+   * This is a callback employed by a Card instance when the latter is being clicked.
+   * Called by Card.onClick()
+   * @memberof Engine
+   * @name onCardFlipOver
+   * @function
+   * @param {number} idIndex - card id index
+   */
+  onCardFlipOver(idIndex) {
+    this.clickCount++;
+    // Add card to the queue.
+    // console.log('Card #' + idIndex + ' flipped face up.');
+    this.flippedCardQueue.push(idIndex);
+    // console.log(this.flippedCardQueue);
 
-  // If there are three cards in queue then it means two initial are mismatching,
-  //   therefore remove initial two from queue and flip them over.
-  if (this.flippedCardQueue.length >= 3
+    // If there are three cards in queue then it means two initial are mismatching,
+    //   therefore remove initial two from queue and flip them over.
+    if (this.flippedCardQueue.length >= 3
       && this.cardSet[this.flippedCardQueue[0]].setId !== this.cardSet[this.flippedCardQueue[1]].setId) {
-    /* console.log('cardSet[~0]: ');
-    console.log(this.cardSet[this.flippedCardQueue[0]]);
-    console.log('cardSet[~1]: ');
-    console.log(this.cardSet[this.flippedCardQueue[1]]); */
-    this.cardSet[this.flippedCardQueue[0]].flipDown();
-    this.cardSet[this.flippedCardQueue[1]].flipDown();
-    this.flippedCardQueue.splice(0, 2);
-    /* console.log('Removed two cards from the queue and have: ');
-    console.log(this.flippedCardQueue); */
-  }
-  // If there are two cards in queue and their setIds are equal
-  //   then hide/remove both from view, queue, and cardSet.
-  if (this.flippedCardQueue.length >= 2
-    && this.cardSet[this.flippedCardQueue[0]].setId === this.cardSet[this.flippedCardQueue[1]].setId) {
-    /* console.log('cardSet[~0]: ');
-    console.log(this.cardSet[this.flippedCardQueue[0]]);
-    console.log('cardSet[~1]: ');
-    console.log(this.cardSet[this.flippedCardQueue[1]]); */
-    // store cards to animate removal
-    let card0 = this.cardSet[this.flippedCardQueue[0]],
+      /* console.log('cardSet[~0]: ');
+      console.log(this.cardSet[this.flippedCardQueue[0]]);
+      console.log('cardSet[~1]: ');
+      console.log(this.cardSet[this.flippedCardQueue[1]]); */
+      this.cardSet[this.flippedCardQueue[0]].flipDown();
+      this.cardSet[this.flippedCardQueue[1]].flipDown();
+      this.flippedCardQueue.splice(0, 2);
+      /* console.log('Removed two cards from the queue and have: ');
+      console.log(this.flippedCardQueue); */
+    }
+    // If there are two cards in queue and their setIds are equal
+    //   then hide/remove both from view, queue, and cardSet.
+    if (this.flippedCardQueue.length >= 2
+      && this.cardSet[this.flippedCardQueue[0]].setId === this.cardSet[this.flippedCardQueue[1]].setId) {
+      /* console.log('cardSet[~0]: ');
+      console.log(this.cardSet[this.flippedCardQueue[0]]);
+      console.log('cardSet[~1]: ');
+      console.log(this.cardSet[this.flippedCardQueue[1]]); */
+      // store cards to animate removal
+      let card0 = this.cardSet[this.flippedCardQueue[0]],
         card1 = this.cardSet[this.flippedCardQueue[1]];
-    this.cardSet[this.flippedCardQueue[0]] = null;
-    this.cardSet[this.flippedCardQueue[1]] = null;
-    /* console.log('Removed two cards from the board and have: ');
-    console.log(this.cardSet); */
-    this.flippedCardQueue.splice(0, 2);
-    /* console.log('Removed two cards from the queue and have: ');
-    console.log(this.flippedCardQueue); */
-    // animate removal
-    setTimeout(() => {
-      card0.hide();
-      card1.hide();
-      // check if any cards remain on the board
-      this.onCardsRemoval();
-    }, 100); // 500
-  }
-};
+      this.cardSet[this.flippedCardQueue[0]] = null;
+      this.cardSet[this.flippedCardQueue[1]] = null;
+      /* console.log('Removed two cards from the board and have: ');
+      console.log(this.cardSet); */
+      this.flippedCardQueue.splice(0, 2);
+      /* console.log('Removed two cards from the queue and have: ');
+      console.log(this.flippedCardQueue); */
+      // animate removal
+      setTimeout(() => {
+        card0.hide();
+        card1.hide();
+        // check if any cards remain on the board
+        this.onCardsRemoval();
+      }, 100); // 500
+    }
+  };
 
-/**
- * This checks if there are any cards on the board. If none then game over panel visualized.
- * Called by Engine.onCardFlipOver()
- * @memberof Engine
- * @name onCardsRemoval
- * @function
- */
-Engine.prototype.onCardsRemoval = function() {
-  // If there are no cards in cardSet
-  //   then game is completed, restart the game.
-  let count = 0;
-  this.cardSet.forEach(function(el){
-    // console.log(el);
-    if (el) count++;
-  });
-  // console.log(count + ' cards remain on the board');
-  if (!count) {
-    // console.log('No cards remaining!');
-    document.getElementById('click-count').innerText = this.clickCount;
-    document.getElementById('game-over').style.display = 'block';
-  }
-};
+  /**
+   * This checks if there are any cards on the board. If none then game over panel visualized.
+   * Called by Engine.onCardFlipOver()
+   * @memberof Engine
+   * @name onCardsRemoval
+   * @function
+   */
+  onCardsRemoval() {
+    // If there are no cards in cardSet
+    //   then game is completed, restart the game.
+    let count = 0;
+    this.cardSet.forEach(function (el) {
+      // console.log(el);
+      if (el) count++;
+    });
+    // console.log(count + ' cards remain on the board');
+    if (!count) {
+      // console.log('No cards remaining!');
+      document.getElementById('click-count').innerText = this.clickCount;
+      document.getElementById('game-over').style.display = 'block';
+    }
+  };
 
-/**
- * This restarts the game.
- * Called by onClick event attached to a button at game over panel in app.js
- * @memberof Engine
- * @name restart
- * @function
- */
-Engine.prototype.restart = function() {
-  document.getElementById('game-over').style.display = 'none';
-  // window.location.reload();
+  /**
+   * This restarts the game.
+   * Called by onClick event attached to a button at game over panel in app.js
+   * @memberof Engine
+   * @name restart
+   * @function
+   */
+  restart() {
+    document.getElementById('game-over').style.display = 'none';
+    // window.location.reload();
 
-  // reset board
-  this.gameBoard.innerHTML = '';
+    // reset board
+    this.gameBoard.innerHTML = '';
 
-  // initialize game board
-  this.init();
-};
+    // initialize game board
+    this.init();
+  };
+}
